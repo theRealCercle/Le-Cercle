2fd35d7b3d2929082ef6e78cec2e616bfd97663d
Felzer9 <46488711+Felzer9@users.noreply.github.com>
2019-01-22 13:29:46 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<?php include_once("includes/db.php") ?>
+<?php session_start(); ?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>le cercle</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
